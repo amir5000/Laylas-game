af4e15aa820be95774da0edca8718e607d734062
aarafat <amirarafat1@gmail.com>
2016-12-13 10:44:33 -0700

    updated the game data for the game

--- gameData.js
+++ gameData.js
@@ -1,24 +1,33 @@
 var gameData = {
     player: {
-        name: "",
-        gender: "",
+        name: 'Amir',
+        gender: 'Female',
         topCoinPosition: 0,
         leftCoinPosition: 0,
         coinHeight: 0,
         coinWidth: 0
     },
-    coin: {
-        topCoinPosition: 0,
-        leftCoinPosition: 0,
-        coinHeight: 0,
-        coinWidth: 0
-    },
-    coinTwo: {
-        topCoinPosition: 0,
-        leftCoinPosition: 0,
-        coinHeight: 0,
-        coinWidth: 0
-    },
-    totalCoins: 0,
-    seconds: 0
+    games: [
+        {
+            id: 'prayGame',
+            name: 'Prayer Game'            
+        },
+        {
+            id: 'quranGame',
+            name: 'Quran Game'
+        },
+        {
+            id: 'alphabetGame',
+            name: 'Alphabet Game'
+        },
+        {
+            id: 'numbersGame',
+            name: 'Numbers Game',
+            score: 0,
+            firstNumber: 9,
+            secondNumber: 9,
+            solution: 0,
+            symbol: '+'
+        }
+    ]
 };
